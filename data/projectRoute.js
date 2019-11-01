168c6248e91ec3cfc1c8d80e6c94b82d6d14dcc7
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-01 12:13:32 -0500

    mvp complete

--- data/projectRoute.js
+++ data/projectRoute.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
             res.status(500).json({error: 'could not retrieve the project'})
         })
 });
-router.get('/:id', (req, res) => {
+router.get('/:id', validateProjectId, (req, res) => {
     const id = req.params.id
     Projects.get(id)
         .then(project => {
@@ -27,7 +27,7 @@ router.get('/:id', (req, res) => {
             res.status(500).json({error: 'could not retrieve the project'})
         })
 });
-router.post('/', (req, res) => {
+router.post('/', validateProjectBody, (req, res) => {
     const body = req.body;
     Projects.insert(body)
         .then(project => {
@@ -38,7 +38,7 @@ router.post('/', (req, res) => {
             res.status(500).json({error: 'could not add a project'})
         })
 });
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateProjectId, (req, res) => {
     const id = req.params.id;
     Projects.remove(id)
         .then(project => {
@@ -49,7 +49,7 @@ router.delete('/:id', (req, res) => {
             res.status(500).json({error: 'could not remove the project from the database'})
         })
 });
-router.put('/:id', (req, res) => {
+router.put('/:id', validateProjectId, validateProjectBody, (req, res) => {
     const id = req.params.id;
     const changes = req.body;
     Projects.update(id, changes)
@@ -61,7 +61,7 @@ router.put('/:id', (req, res) => {
             res.status(500).json({error: 'could not update the project'})
         })
 })
-router.get('/:id/actions', (req, res) => {
+router.get('/:id/actions', validateProjectId, (req, res) => {
     const ProjectId = req.params.id;
     Projects.get(ProjectId)
         .then(action => {
@@ -73,7 +73,18 @@ router.get('/:id/actions', (req, res) => {
         })
 })
 /////////////////////// actions operations/////////////////
-router.post('/:id/actions', (req, res) => {
+router.get('/:id/actions/:id', validateActionId, (req, res) => {
+    const id = req.params.id;
+    Actions.get(id)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({error: 'unable to get the action by id'})
+        })
+})
+router.post('/:id/actions', validateProjectId, validateActionBody, (req, res) => {
     const body = req.body;
     
     console.log(body)
@@ -86,7 +97,7 @@ router.post('/:id/actions', (req, res) => {
             res.status(500).json({error: 'could not add actions'})
         })
 });
-router.put('/:id/actions/:id', (req, res) => {
+router.put('/:id/actions/:id', validateActionId, validateActionBody, (req, res) => {
     const changes = req.body;
     const id = req.params.id;
     Actions.update(id, changes)
@@ -98,7 +109,7 @@ router.put('/:id/actions/:id', (req, res) => {
             res.status(500).json({error: 'could not update the action'})
         })
 })
-router.delete('/:id/actions/:id', (req, res) => {
+router.delete('/:id/actions/:id', validateActionId, (req, res) => {
     const id = req.params.id;
     Actions.remove(id)
         .then(action => {
@@ -109,6 +120,59 @@ router.delete('/:id/actions/:id', (req, res) => {
             res.status(500).json({error: 'the action could not be deleted'})
         })
 });
+
+///////////////middleware////////////////
+
+function validateProjectId(req, res, next) {
+    const id = req.params.id;
+    Projects.get(id)
+        .then(project => {
+            if(!project){
+                return res.status(400).json({message: 'invalid project id'})
+            } else {
+                next();
+            }
+        })
+        .catch(err => {
+            console.log('validateProjectId', err)
+            res.status(500).json({error: 'could not validate the id'})
+        })
+};
+function validateActionId(req, res, next){
+    const id = req.params.id;
+    Actions.get(id)
+        .then(action => {
+            if(!action){
+                return res.status(400).json({error: 'invalid action id'})
+            } else {
+                next();
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: 'could not validate the action id'})
+        })
+};
+function validateProjectBody(req, res, next){
+    const body = req.body;
+    if(!body){
+        res.status(400).json({message: 'missing project data'})
+    } else if(!body.name || !body.description){
+        res.status(400).json({message: 'missing required name or description field'})
+    } else{
+        next();
+    }
+};
+function validateActionBody(req, res, next){
+    const body = req.body;
+    if(!body){
+        res.status(400).json({message: 'missing action data'})
+    } else if (!body.project_id || !body.description || !body.notes){
+        res.status(400).json({message: 'missing required project_id, description, or notes data'})
+    } else {
+        next();
+    }
+}
 
 
 module.exports = router;
